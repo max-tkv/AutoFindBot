c71f5ef067e8a9a792eb0d7e8f7e9041a25d4722
Терехов Максим Анатольевич <TerehovMA@moffice.akbars.ru>
2023-05-13 12:39:45 +0300

    Add new migration db (rename column)

---
+++ AutoFindBot.Storage/Migrations/20230513093554_RenameColumns.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoFindBot.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Source",
+                table: "SourceChecks",
+                newName: "SourceType");
+
+            migrationBuilder.RenameColumn(
+                name: "Source",
+                table: "Cars",
+                newName: "SourceType");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cars_Title_Price_Year_Source",
+                table: "Cars",
+                newName: "IX_Cars_Title_Price_Year_SourceType");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "UserFilters",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserFilters",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "SourceChecks",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "SourceChecks",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "Payments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Payments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "Cars",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublishedAt",
+                table: "Cars",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Cars",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "Actions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Actions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "current_timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SourceType",
+                table: "SourceChecks",
+                newName: "Source");
+
+            migrationBuilder.RenameColumn(
+                name: "SourceType",
+                table: "Cars",
+                newName: "Source");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cars_Title_Price_Year_SourceType",
+                table: "Cars",
+                newName: "IX_Cars_Title_Price_Year_Source");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "Users",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "UserFilters",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserFilters",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "SourceChecks",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "SourceChecks",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "Payments",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Payments",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "Cars",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublishedAt",
+                table: "Cars",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Cars",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedDateTime",
+                table: "Actions",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Actions",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "current_timestamp");
+        }
+    }
+}
