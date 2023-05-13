c6ef8080be1aeadaafe5c671351702073e12e648
Терехов Максим Анатольевич <TerehovMA@moffice.akbars.ru>
2023-05-13 11:58:52 +0300

    Refactoring CaptchaSolutionsService

--- AutoFindBot/Services/CaptchaSolutionsService.cs
+++ AutoFindBot/Services/CaptchaSolutionsService.cs
@@ -101,13 +101,20 @@ public class CaptchaSolutionsService : ICaptchaSolutionsService
 
         await _webDriverService.FindElementByIdAsync("app");
 
-        SetKeyAutoProbegCookies(httpRequestMessage, driver.Manage().Cookies.AllCookies);
+        SetNewCookies(httpRequestMessage, driver.Manage().Cookies.AllCookies);
     }
 
-    public Task SolutionDromAsync(HttpRequestMessage request)
+    public async Task SolutionDromAsync(HttpRequestMessage request)
     {
-        throw new NotImplementedException();
+        using var driver = _webDriverService.CreateChromeDriver();
+        driver.Navigate().GoToUrl(request.RequestUri?.ToString());
+
+        await _webDriverService.FindElementByCssSelectorAsync("div[data-app-root='bulls-list-auto']");
+
+        SetNewCookies(request, driver.Manage().Cookies.AllCookies);
     }
+
+    #region Приватные методы
 
     private void SetRedirect(HttpRequestMessage httpRequest, HttpMethod newMethod, string newUrl)
     {
@@ -118,24 +125,34 @@ public class CaptchaSolutionsService : ICaptchaSolutionsService
         httpRequest.Method = newMethod;
         httpRequest.RequestUri = new Uri(newUrl);
     }
-    
-    private void SetKeyAutoProbegCookies(HttpRequestMessage httpRequestMessage, ReadOnlyCollection<Cookie> cookiesAllCookies)
-    {
-        var cookieValue = string.Join("; ", cookiesAllCookies.OrderBy(x => x.Name).Select(cookie => $"{cookie.Name}={cookie.Value}"));
-        httpRequestMessage.Headers.Add("Cookie", cookieValue);
-    }
 
-    private void SetAutoRuCookiesAndToken(HttpRequestMessage httpRequest, ReadOnlyCollection<Cookie> driverCookies)
+    private void SetAutoRuCookiesAndToken(
+        HttpRequestMessage httpRequest, 
+        ReadOnlyCollection<Cookie> driverCookies)
     {
-        var cookieValue = string.Join("; ", driverCookies.Select(cookie => $"{cookie.Name}={cookie.Value}"));
-        cookieValue += "; yandex_login=max.tkv; Session_id=3:1683636553.5.0.1674329708913:lBY-Lg:31.1.1:czoxNjY4ODg5OTk5NDY5OmxCWS1MZzo0NA.2:1|1370033367.-1.2|385032602.1068.2.2:1068|61:10013073.835889.KOkFiqyyTGSqqXv5GqmuUm-8qMg; " +
-                       "ys=udn.cDptYXgudGt2#c_chck.3875977813; mda2_beacon=1683636553303; " +
-                       "sso_status=sso.passport.yandex.ru:synchronized; " +
-                       "_yasc=X23JRT14qotbUdi5k+Ba23UzhUJN6lkNs8ZZdaJROgdeAS95rlxo8Tw/Z4+K2TE=; gradius=0; " +
-                       "cycada=8jSDnd6bpcmIU+hlcyNrire9a+uIKehYp+cFw1YoWmI=; _ym_d=1683636760; count-visits=4; " +
-                       "from_lifetime=1683637032901";
-        httpRequest.Headers.Add("Cookie", cookieValue);
+        var additionalCookies =
+            "yandex_login=max.tkv; Session_id=3:1683636553.5.0.1674329708913:lBY-Lg:31.1.1:czoxNjY4ODg5OTk5NDY5OmxCWS1MZzo0NA.2:1|1370033367.-1.2|385032602.1068.2.2:1068|61:10013073.835889.KOkFiqyyTGSqqXv5GqmuUm-8qMg; " +
+            "ys=udn.cDptYXgudGt2#c_chck.3875977813; mda2_beacon=1683636553303; " +
+            "sso_status=sso.passport.yandex.ru:synchronized; " +
+            "_yasc=X23JRT14qotbUdi5k+Ba23UzhUJN6lkNs8ZZdaJROgdeAS95rlxo8Tw/Z4+K2TE=; gradius=0; " +
+            "cycada=8jSDnd6bpcmIU+hlcyNrire9a+uIKehYp+cFw1YoWmI=; _ym_d=1683636760; count-visits=4; " +
+            "from_lifetime=1683637032901";
+        SetNewCookies(httpRequest, driverCookies, additionalCookies);
         httpRequest.Headers.Add("x-csrf-token", driverCookies.Single(x => x.Name == "_csrf_token").Value);
+    }
+    
+    private void SetNewCookies(
+        HttpRequestMessage httpRequestMessage, 
+        ReadOnlyCollection<Cookie> cookiesAllCookies, 
+        string? additionalCookies = null)
+    {
+        var cookieValue = string.Join("; ", cookiesAllCookies.OrderBy(x => x.Name)
+            .Select(cookie => $"{cookie.Name}={cookie.Value}"));
+        if (!string.IsNullOrWhiteSpace(additionalCookies))
+        {
+            cookieValue += $"; {additionalCookies}";
+        }
+        httpRequestMessage.Headers.Add("Cookie", cookieValue);
     }
 
     private async Task<byte[]> DownloadImageAsync(string imageUrl)
@@ -162,7 +179,7 @@ public class CaptchaSolutionsService : ICaptchaSolutionsService
         }
     }
     
-    public string GetCaptchaImageUrl(string html)
+    private string GetCaptchaImageUrl(string html)
     {
         string pattern = "<div class=\"AdvancedCaptcha-View\"><img src=\"(.*?)\".*?><\\/div>";
         var match = Regex.Match(html, pattern);
@@ -173,4 +190,6 @@ public class CaptchaSolutionsService : ICaptchaSolutionsService
         
         throw new CaptchaSolutionsServiceException(CaptchaInvariants.DetectionErrorInHtml);
     }
+
+    #endregion
 }
